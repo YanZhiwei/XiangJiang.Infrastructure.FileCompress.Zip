45dd4d770b3c064830f3649d084514e6ece6e28f
言志伟 <Yan.Zhiwei@hotmail.com>
2020-09-03 19:02:21 +0800

    add unittest

---
+++ XiangJiang.Infrastructure.FileCompress.ZipTests/ZipFileCompressTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XiangJiang.Infrastructure.Abstractions;
+
+namespace XiangJiang.Infrastructure.FileCompress.Zip.Tests
+{
+    [TestClass]
+    public class ZipFileCompressTests
+    {
+        private IFileCompressProvider _fileCompressProvider;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _fileCompressProvider = new ZipFileCompress();
+        }
+
+        [TestMethod]
+        public void CompressTest()
+        {
+            var compressFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+            var zipFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AnyDesk.zip");
+            _fileCompressProvider.Compress(compressFolder, zipFile);
+            Assert.IsTrue(File.Exists(zipFile));
+        }
+
+        [TestMethod]
+        public void ExtractTest()
+        {
+            var extractFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AnyDesk");
+            var zipFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "AnyDesk.zip");
+            _fileCompressProvider.Extract(zipFile, extractFolder);
+            Assert.IsTrue(Directory.Exists(extractFolder));
+            Assert.IsTrue(File.Exists(Path.Combine(extractFolder, "AnyDesk.exe")));
+        }
+    }
+}
